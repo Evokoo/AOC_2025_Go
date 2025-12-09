3526b10dcebc563646367210585e6f944faf0b1a
Evokoo <evanjohncooper@gmail.com>
2025-12-09 20:33:20 +1100

    PrintGridToFile added

--- utils/files.go
+++ utils/files.go
@@ -12,3 +12,28 @@ func ReadFile(title string) string {
 	}
 	return string(data)
 }
+
+// ========================
+// PRINT GRID TO FILE
+// ========================
+func PrintGridToFile(grid [][]string, filename string) error {
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	for y := range grid {
+		for x := range grid[y] {
+			_, err := f.WriteString(grid[y][x])
+			if err != nil {
+				return err
+			}
+		}
+		_, err := f.WriteString("\n")
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
